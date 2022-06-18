dd49f4ab60057a1f4a184a7d969f8154fe038340
moeez <moeez.kokab@gmail.com>
2022-06-18 14:05:17 +0500

    second commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,17 @@
-import { StyleSheet, Text, View, Image , Dimensions} from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const Card = () => {
     return (
-        <View style={{marginTop:10}}>
-            <Image source={{ uri: "https://images.unsplash.com/photo-1511300636408-a63a89df3482?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" }
-
-            }
+        <View style={{ marginTop: 10 }}>
+            <Image source={{ uri: "https://images.unsplash.com/photo-1511300636408-a63a89df3482?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" }}
                 style={styles.image} />
-            <View style={{flexDirection:"row",marginLeft:5}}>
+            <View style={{ flexDirection: "row", marginLeft: 5 }}>
                 <MaterialIcons name="account-circle" size={50} color="black" />
-                <View style={{marginLeft:5}}>
-                    <Text style={{fontSize:20,width:Dimensions.get("screen").width-50}} 
-                    ellipsizeMode="tail" 
-                    numberOfLines={2}>
+                <View style={{ marginLeft: 5 }}>
+                    <Text style={{ fontSize: 20, width: Dimensions.get("screen").width - 50 }}
+                        ellipsizeMode="tail"
+                        numberOfLines={2}>
                         this is my video this is my video this is my video this is my video this is my video this is my video this is my video this is my video this is my video this is my video </Text>
                     <Text>cccccccccc</Text>
                 </View>
--- src/screen/Search.js
+++ src/screen/Search.js
@@ -1,14 +1,15 @@
-import { View, TextInput } from 'react-native';
+import { View, TextInput, ScrollView, Touchable } from 'react-native';
 import { AntDesign, Feather } from '@expo/vector-icons';
 import { useState } from 'react';
 import Constants from 'expo-constants';
+import MiniCard from '../components/MiniCard';
 
 const Search = () => {
     const [input, setInput] = useState()
     return (
         <View style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
             <View style={{
-                padding:5,
+                padding: 5,
                 flexDirection: "row",
                 justifyContent: "space-between",
                 shadowColor: "#000",
@@ -18,7 +19,7 @@ const Search = () => {
                 },
                 shadowOpacity: 0.22,
                 shadowRadius: 2.22,
-                
+
                 elevation: 3,
                 backgroundColor: "white"
             }}>
@@ -26,10 +27,25 @@ const Search = () => {
                 <TextInput
                     value={input}
                     onChangeText={(txt) => setInput(txt)}
-                    style={{ width: "80%", borderWidth:2 , backgroundColor:"#e6e6e6",borderEndWidth:1,borderLeftWidth:1 }}
+                    style={{ width: "80%", borderWidth: 2, backgroundColor: "#e6e6e6", borderEndWidth: 1, borderLeftWidth: 1 }}
                 />
                 <Feather name="send" size={24} color="black" />
             </View>
+
+            <ScrollView>
+                
+                    <MiniCard />
+                    <MiniCard />
+                    <MiniCard />
+                    <MiniCard />
+                    <MiniCard />
+                    <MiniCard />
+                    <MiniCard />
+                    <MiniCard />
+               
+            </ScrollView>
+
+
 
         </View>
     )
