d2642541ee41842d0a29af9ddb3bd31be997f7bb
moeez <moeez.kokab@gmail.com>
2022-06-18 18:24:37 +0500

    second commit

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,14 +1,14 @@
 import { View, Image, Text, Dimensions } from "react-native";
 
-const MiniCard = () => {
+const MiniCard = (props) => {
     return (
-        <View style={{ flexDirection: "row",margin:10,marginBottom:0 }}>
-            <Image source={{ uri: "https://images.unsplash.com/photo-1511300636408-a63a89df3482?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" }}
+        <View style={{ flexDirection: "row", margin: 10, marginBottom: 0 }}>
+            <Image source={{ uri: `https://i.ytimg.com/vi/${props.videoId}/maxresdefault.jpg` }}
                 style={{
                     height: 100,
                     width: "45%"
                 }} />
-            <View style={{margin:5}}>
+            <View style={{ margin: 5 }}>
                 <Text
                     style=
                     {{
@@ -19,14 +19,17 @@ const MiniCard = () => {
                     ellipsizeMode="tail"
                     numberOfLines={2}
                 >
-                    This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video This is my video
+                    {props.title}
                 </Text>
-                <Text style={{fontSize:12}}>jjjj</Text>
+                <Text style={{ fontSize: 12 }}>
+                    {props.channel}
+                </Text>
             </View>
 
 
         </View>
     )
+    
 }
 
 export default MiniCard
--- src/screen/Search.js
+++ src/screen/Search.js
@@ -1,11 +1,30 @@
-import { View, TextInput, ScrollView, Touchable } from 'react-native';
+import { View, TextInput, ScrollView, FlatList , ActivityIndicator } from 'react-native';
 import { AntDesign, Feather } from '@expo/vector-icons';
 import { useState } from 'react';
 import Constants from 'expo-constants';
 import MiniCard from '../components/MiniCard';
 
+
+// https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=Ironman&type=video&key=AIzaSyAtuydSMp1DlLb0l04cK6iz8gawQ9Kpee8
+
 const Search = () => {
     const [input, setInput] = useState()
+    const [miniCard, setMiniCard] = useState([])
+    const [loading,SetLoading] = useState(false)
+
+    const fetchData = () => {
+        SetLoading(true)
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${input}&type=video&key=AIzaSyAtuydSMp1DlLb0l04cK6iz8gawQ9Kpee8`)
+            .then(res => res.json()
+                .then(data => {
+                    SetLoading(false)
+                    setMiniCard(data.items)
+                    console.log(miniCard)
+                    
+                }
+
+                ))
+    }
     return (
         <View style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
             <View style={{
@@ -29,26 +48,26 @@ const Search = () => {
                     onChangeText={(txt) => setInput(txt)}
                     style={{ width: "80%", borderWidth: 2, backgroundColor: "#e6e6e6", borderEndWidth: 1, borderLeftWidth: 1 }}
                 />
-                <Feather name="send" size={24} color="black" />
+                <Feather name="send" size={24} color="black" onPress={() => fetchData()
+                } />
             </View>
-
-            <ScrollView>
-                
-                    <MiniCard />
-                    <MiniCard />
-                    <MiniCard />
-                    <MiniCard />
-                    <MiniCard />
-                    <MiniCard />
-                    <MiniCard />
-                    <MiniCard />
-               
-            </ScrollView>
+          {loading ?<ActivityIndicator size="large" color='red' style={{marginTop:10}}/>:null}  
+            <FlatList
+                data={miniCard}
+                renderItem={({item}) => {
+                    return <MiniCard
+                    videoId={item.id.videoId}
+                    title={item.snippet.title}
+                    channel={item.snippet.channelTitle}
+                    />
+                }}
+                keyExtractor={(item)=>{item.id.videoId}}
+            />
 
 
 
         </View>
-    )
+    ) 
 }
 
 export default Search;
